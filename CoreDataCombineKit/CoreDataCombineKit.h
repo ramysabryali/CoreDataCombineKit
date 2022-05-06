87fd3839db9d9e428dc091a12ee06277bd206d35
Ramy Sabry <ramy.sabry.external@atos.net>
2022-05-06 04:31:13 +0200

    Initial Commit

---
+++ CoreDataCombineKit/CoreDataCombineKit.h
@@ -0,0 +1,18 @@
+//
+//  CoreDataCombineKit.h
+//  CoreDataCombineKit
+//
+//  Created by Ramy Sabry on 06/05/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreDataCombineKit.
+FOUNDATION_EXPORT double CoreDataCombineKitVersionNumber;
+
+//! Project version string for CoreDataCombineKit.
+FOUNDATION_EXPORT const unsigned char CoreDataCombineKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataCombineKit/PublicHeader.h>
+
+
---
+++ CoreDataCombineKitTests/CoreDataCombineKitTests.swift
@@ -0,0 +1,33 @@
+//
+//  CoreDataCombineKitTests.swift
+//  CoreDataCombineKitTests
+//
+//  Created by Ramy Sabry on 06/05/2022.
+//
+
+import XCTest
+@testable import CoreDataCombineKit
+
+class CoreDataCombineKitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
